ecf4b6484ac88d2c9a231657bb3ba08bb35344bc
Md Babla Islam <bilasislam5643@gmail.com>
2025-01-09 00:15:37 +0600

    Initial Commit

---
+++ Share Point/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Share Point
+//
+//  Created by Dibyo sarkar on 9/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Share Point/Share_PointApp.swift
@@ -0,0 +1,17 @@
+//
+//  Share_PointApp.swift
+//  Share Point
+//
+//  Created by Dibyo sarkar on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Share_PointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
